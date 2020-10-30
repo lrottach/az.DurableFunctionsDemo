c4309aba62106ba25c1249ce7b9089606cb6e30b
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2020-10-30 13:19:06 +0100

    feat: implemented authentication

--- src/Authentication/AuthHandler.cs
+++ src/Authentication/AuthHandler.cs
@@ -5,5 +5,20 @@ using System.Threading;
 
 namespace DarkContoso.DurableFunction.Authentication
 {
-    
+    public class AuthHandler : DelegatingHandler
+    {
+        private IAuthenticationProvider _authenticationProvider;
+
+        public AuthHandler(IAuthenticationProvider authenticationProvider, HttpMessageHandler innerHandler)
+        {
+            InnerHandler = innerHandler;
+            _authenticationProvider = authenticationProvider;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await _authenticationProvider.AuthenticateRequestAsync(request);
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
 }
--- src/DurableFunctionsOrchestration.cs
+++ src/DurableFunctionsOrchestration.cs
@@ -19,8 +19,6 @@ namespace DarkContoso.DurableFunction.Skills
 
             // Replace "hello" with the name of your Durable Activity Function.
             outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestration_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestration_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableFunctionsOrchestration_Hello", "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
